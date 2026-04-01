97605cc0e9d69e695c0db2e7da12ef00bfe516a2
DemiusLs <dloss320@gmail.com>
2026-04-02 00:56:34 +0200

    feat: fetch books modified

--- contexts/BooksContext.js
+++ contexts/BooksContext.js
@@ -1,6 +1,7 @@
 import {createContext, useState, useEffect} from "react";
-import { ID } from "react-native-appwrite";
+import { ID, Permission, Query, Role } from "react-native-appwrite";
 import { databases } from "../lib/appwrite";
+import { useUser } from "../hooks/useUser";
 
 const DATABASE_ID = process.env.DATABASE_ID;
 const COLLECTION_ID= process.env.COLLECTION_ID;
@@ -9,10 +10,19 @@ export const BooksContext = createContext();
 
 export function BooksProvider ({children}){
     const [books, setBooks] = useState([]);
+    const {user} = useUser();
 
     async function fetchBooks() {
         try {
-            
+            const response = await databases.listDocuments(
+            DATABASE_ID, 
+            COLLECTION_ID,
+            [
+            Query.equal('userId', user.$id)
+            ]
+        )
+        setBooks(response.documents);
+        console.log(response.documents);            
         } catch (error) {
             console.error(error.message);
         }
@@ -29,6 +39,17 @@ export function BooksProvider ({children}){
 
     async function createBook(data) {
         try {
+            const newBook = await databases.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {...data, userId : user.$id},
+                [
+                    Permission.read(Role.user(user.$id)),
+                    Permission.update(Role.user(user.$id)),
+                    Permission.delete(Role.user(user.$id)),
+                ]
+            )
             
         } catch (error) {S
             console.error(error.message);
@@ -42,6 +63,16 @@ export function BooksProvider ({children}){
             console.error(error.message);
         }
     }
+
+    useEffect(() => {
+
+        if (user) {
+        fetchBooks()
+        } else {
+        setBooks([])
+        }
+
+    }, [user])
 
     return(
             <BooksContext.Provider value={{books, fetchBooks ,fetchBook, createBook, deleteBook}}>
